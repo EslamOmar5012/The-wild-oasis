3ff96d02c59e3d4a92213086a976279b222e457f
Eslam Omar <eslam.O.elkhabery@gmail.com>
2026-02-04 03:16:01 +0200

    Implement logout functionality with loading state and integrate into Header component

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,10 +1,14 @@
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
 import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "../../hooks/useLogout";
+import Spinner from "../../ui/Spinner";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function Logout() {
+  const [isLoading, logout] = useLogout();
   return (
-    <ButtonIcon>
-      <HiArrowRightOnRectangle />
+    <ButtonIcon disabled={isLoading} onClick={logout}>
+      {isLoading ? <SpinnerMini /> : <HiArrowRightOnRectangle />}
     </ButtonIcon>
   );
 }
